c8375d6712763d0938083b84353eca4ae272f770
Henrry-Maximo <henrrylimadasilva@gmail.com>
2024-06-09 23:21:43 -0300

    first commit

---
+++ GLPI-Analytics-Pro/src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <p>Página / Dashboard</p>
+        <span>Dashboard</span>
+        <search>
+          <input type="text" placeholder="Pesquise aqui..." />
+        </search>
+        <div>
+          <ul>
+            <li>...</li>
+            <li>...</li>
+          </ul>
+        </div>
+      </header>
+      <aside></aside>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+// export default App;
