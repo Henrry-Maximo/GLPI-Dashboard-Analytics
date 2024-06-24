4d978b78f34d4ba59de1902a9cc0a77696ae611e
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-06-24 09:53:28 -0300

    feat: connection database glpi and list of user

--- api-rest-GLPI/src/controllers/userController.ts
+++ api-rest-GLPI/src/controllers/userController.ts
@@ -1,9 +1,17 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance } from "fastify";
+import { createConnection } from "../database/db";
 
 export async function userController(app: FastifyInstance) {
-  app.get('/', () => {
-    return (
-      "hello wolrd!"
-    )
-  })
+  app.get("/", async (req, reply) => {
+    try {
+      const conn = await createConnection();
+      const [rows] = await conn.query("SELECT name FROM glpi_users;");
+      await conn.end();
+      return reply.status(200).send(rows);
+    } catch (err) {
+      return reply.status(500).send({
+        error: "Internal Server Error",
+      });
+    }
+  });
 }
