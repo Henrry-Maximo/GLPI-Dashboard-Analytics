336d13908b873124e02f5bda749c2c89b487129e
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-06-27 11:18:05 -0300

    feat: view route

--- api-rest-GLPI/src/routes/routes.ts
+++ api-rest-GLPI/src/routes/routes.ts
@@ -1,10 +1,22 @@
-import { FastifyInstance } from 'fastify'
-import { userController } from "../controllers/userController"
-import { ticketController } from '../controllers/ticketController'
-import { categorieController } from '../controllers/categorieController'
+import { FastifyInstance } from "fastify";
+import { userController } from "../controllers/userController";
+import { ticketController } from "../controllers/ticketController";
+import { categorieController } from "../controllers/categorieController";
 
 export async function routes(app: FastifyInstance) {
-  app.register(userController, { prefix: '/user' })
-  app.register(ticketController, { prefix: '/ticket' })
-  app.register(categorieController, { prefix: 'categorie' })
+  app.get("/", async (req, reply) => {
+    try {
+      const routes = app.printPlugins();
+      reply.status(200).send(routes);
+      // return reply.status(200).send();
+    } catch (err) {
+      return reply.status(500).send({
+        error: "Internal Server Error",
+      });
+    }
+  });
+
+  app.register(userController, { prefix: "/user" });
+  app.register(ticketController, { prefix: "/ticket" });
+  app.register(categorieController, { prefix: "categorie" });
 }
