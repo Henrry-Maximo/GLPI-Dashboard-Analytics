239b3740d8cf36c4c7d5181d0c1eb404dd3d7ba3
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-06-27 11:03:25 -0300

    feat: treatment of data and returning of category count

--- api-rest-GLPI/src/controllers/categorieController.ts
+++ api-rest-GLPI/src/controllers/categorieController.ts
@@ -3,9 +3,28 @@ import { createConnection } from "../database/db";
 
 export async function categorieController(app: FastifyInstance) {
   app.get("/", async (req, reply) => {
-    const db = await createConnection();
-    const [rows] = await db.execute("SELECT name FROM glpi_itilcategories;");
+    try {
+      const db = await createConnection();
+    const [rows] = await db.execute(
+      "SELECT name FROM glpi_itilcategories"
+    );
 
     return reply.status(200).send(rows);
+    } catch (err) {
+      return reply.status(400).send(err);
+    }
+  });
+
+  app.get("/categories-by-count", async (req, reply) => {
+    try {
+      const db = await createConnection();
+    const [rows] = await db.execute(
+      "SELECT COUNT(id) as count FROM glpi_itilcategories;"
+    );
+
+    return reply.status(200).send(rows);
+    } catch (err) {
+      return reply.status(400).send(err);
+    }
   });
 }
