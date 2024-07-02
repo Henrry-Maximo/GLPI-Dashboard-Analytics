04fa9be0c1e6df5baeaf86169e27581ee3192f4d
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-02 10:30:08 -0300

    fix: code route ticket and comment

--- api-rest-GLPI/src/controllers/ticketController.ts
+++ api-rest-GLPI/src/controllers/ticketController.ts
@@ -2,12 +2,14 @@ import { FastifyInstance } from "fastify";
 import { createConnection } from "../database/db";
 
 export async function ticketController(app: FastifyInstance) {
+  // retornar todos os chamados por nome
   app.get("/tickets", async (req, reply) => {
     const db = await createConnection();
     const [rows] = await db.query("SELECT name FROM glpi_tickets;");
     return reply.status(200).send(rows);
   });
 
+  // retornar número de chamados por status
   app.get("/tickets-by-count", async (req, reply) => {
     const db = await createConnection();
     const [rows] = await db.query(`
@@ -16,12 +18,14 @@ export async function ticketController(app: FastifyInstance) {
         COUNT(CASE WHEN status = 1 THEN 1 END) AS total_abertos,
         COUNT(CASE WHEN status = 2 THEN 1 END) AS total_atribuidos,
         COUNT(CASE WHEN status = 4 THEN 1 END) AS total_pendentes,
-        COUNT(CASE WHEN status = 5 THEN 1 END) AS total_finalizados
+        COUNT(CASE WHEN status = 5 THEN 1 END) AS total_solucionados,
+        COUNT(CASE WHEN status = 6 THEN 1 END) AS total_fechados
       FROM glpi_tickets;
     `);
     return reply.status(200).send(rows);
   });
 
+  // retornar número de chamado por urgência
   app.get("/tickets-by-count-urgency", async (req, reply) => {
     const db = await createConnection();
     const [rows] = await db.query(`
@@ -36,18 +40,20 @@ export async function ticketController(app: FastifyInstance) {
     return reply.status(200).send(rows);
   });
 
+  // retornar quantiade de chamados associados a uma categoria
   app.get("/ticket-by-categorie", async (req, reply) => {
     const db = await createConnection();
     const [rows] = await db.query(
-      `SELECT c.completename AS categoria, COUNT(t.id) AS quantidade_tickets FROM glpi_tickets t JOIN glpi_itilcategories c ON t.itilcategories_id = c.id GROUP BY c.completename ORDER BY quantidade_tickets DESC;`
+      `SELECT c.completename AS category, COUNT(t.id) AS quantity_tickets FROM glpi_tickets t JOIN glpi_itilcategories c ON t.itilcategories_id = c.id GROUP BY c.completename ORDER BY quantity_tickets DESC;`
     );
     return reply.status(200).send(rows);
   });
 
+  // retornar últimos 10 chamados por categoria (id, name, status, date, category)
   app.get("/ticket-last-by-categorie", async (req, reply) => {
     const db = await createConnection();
     const [rows] = await db.query(
-      "SELECT t.id, t.name, t.status as status, t.date as date, c.name AS category_name FROM glpi_tickets AS t INNER JOIN glpi_itilcategories AS c ON t.itilcategories_id = c.id WHERE t.status IN (1,2,3,4) ORDER BY t.id DESC LIMIT 10;"
+      "SELECT t.id, t.name, t.status as status, t.date as date, c.name AS category_name FROM glpi_tickets AS t INNER JOIN glpi_itilcategories AS c ON t.itilcategories_id = c.id WHERE t.status IN (1,2,3,4,5) ORDER BY t.id DESC LIMIT 10;"
     );
     return reply.status(200).send(rows);
   });
