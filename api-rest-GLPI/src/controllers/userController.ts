e99a3771a47e8f7ae155d295c3df8a65b04b983e
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-11 11:09:34 -0300

    feat: add /users-by-tickets route for display quantity of tickets by users

--- api-rest-GLPI/src/controllers/userController.ts
+++ api-rest-GLPI/src/controllers/userController.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { createConnection } from "../database/db";
 
 export async function userController(app: FastifyInstance) {
-  // retornar o nome de todos os usuários 
+  // retornar o nome de todos os usuários
   app.get("/users", async (req, reply) => {
     try {
       const conn = await createConnection();
@@ -20,11 +20,31 @@ export async function userController(app: FastifyInstance) {
   app.get("/users-by-count", async (req, reply) => {
     try {
       const conn = await createConnection();
-      const [rows] = await conn.query("SELECT COUNT(id) AS Total FROM glpi_users;");
+      const [rows] = await conn.query(
+        "SELECT COUNT(id) AS Total FROM glpi_users;"
+      );
       await conn.end();
-      return reply.status(200).send(rows)
+      return reply.status(200).send(rows);
     } catch (err) {
       return reply.status(500).send({ error: "Internal Server Error" });
     }
+  });
+
+  // retornar número de chamados por usuário
+  app.get("/users-by-tickets", async (req, reply) => {
+    const db = await createConnection();
+    const [rows] = await db.query(`
+    SELECT 
+      u.name AS usuario,
+      COUNT(t.id) AS quantidade_chamados
+    FROM 
+      glpi_tickets t
+    JOIN 
+      glpi_users u ON t.users_id_recipient = u.id
+    GROUP BY 
+      u.name
+    ORDER BY 
+      quantidade_chamados DESC;`);
+    return reply.status(200).send(rows);
   });
 }
