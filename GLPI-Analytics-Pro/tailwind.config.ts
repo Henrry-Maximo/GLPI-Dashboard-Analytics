a93dd17eece9f62116b36f2aea13e6a09a1f763b
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-25 12:21:27 -0300

    ✨ feat: Configured Tailwind CSS with Vite

---
+++ GLPI-Analytics-Pro/tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: { colors: {
+      purple: "#a34532"
+    } },
+  },
+  plugins: [],
+};
