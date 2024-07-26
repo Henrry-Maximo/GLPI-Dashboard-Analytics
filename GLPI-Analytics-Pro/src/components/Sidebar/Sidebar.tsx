c98a8e7bfbf2e8031e4d56b77d8617b9148087b2
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-07-26 15:17:09 -0300

    🎨  style: using Tailwind CSS in component Sidebar

--- GLPI-Analytics-Pro/src/components/Sidebar/Sidebar.tsx
+++ GLPI-Analytics-Pro/src/components/Sidebar/Sidebar.tsx
@@ -12,39 +12,47 @@ import {
 
 export default function Sidebar() {
   return (
-    <div className="flex flex-col p-3 w-60 m-0 text-gray-100 bg-gray-600">
-      <div className="flex flex-col flex-1 justify-between gap-4 m-4 p-1">
-        <div className="flex flex-col gap-2">
-          <div>
+<div className="flex flex-col p-6 w-60 m-0 text-gray-100 bg-gray-600 justify-between h-full">
+      <nav className="flex flex-col gap-4">
+        <ul className="flex flex-col space-y-2">
+          <li className="flex items-center gap-2">
             <PresentationChart size={16} />
-            Dashboard
-          </div>
-          <div>
+            <span>Dashboard</span>
+          </li>
+          <li className="flex items-center gap-2">
             <Ticket size={16} />
-            Chamados
-          </div>
-          <div>
+            <span>Chamados</span>
+          </li>
+          <li className="flex items-center gap-2">
             <TrendUp size={16} />
-            Estatísticas
-          </div>
-          <div>
+            <span>Estatísticas</span>
+          </li>
+          <li className="flex items-center gap-2">
             <ChartPie size={16} />
-            Análises
-          </div>
-          <div>
+            <span>Análises</span>
+          </li>
+          <li className="flex items-center gap-2">
             <BellSimple size={16} />
-            Notificações
-          </div>
-          <div>
+            <span>Notificações</span>
+          </li>
+          <li className="flex items-center gap-2">
             <Question size={16} />
-            Ajuda
-          </div>
-        </div>
-        <div>
-          <div><SignOut size={16} />Logout</div>
-          <div><ToggleRight size={16} />Light Mode</div>
-        </div>
-      </div>
+            <span>Ajuda</span>
+          </li>
+        </ul>
+      </nav>
+      <nav className="flex flex-col gap-4">
+        <ul className="flex flex-col space-y-2">
+          <li className="flex items-center gap-2">
+            <SignOut size={16} />
+            <span>Logout</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <ToggleRight size={16} />
+            <span>Light Mode</span>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
