5a7639d7aa7a11e31a380f734fabd6810b980b18
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-08-16 17:00:33 -0300

    ✨ feat: add ticket details section

--- GLPI-Analytics-Pro/src/pages/home/index.tsx
+++ GLPI-Analytics-Pro/src/pages/home/index.tsx
@@ -21,6 +21,22 @@ export default function Home() {
     setMenuOpen(!menuOpen)
   }
 
+  const ticketData = {
+    id: 5,
+    entities_id: 1,
+    name: 'Teste Wilka 4',
+    date_creation: '2022-04-20T15:56:53.000Z',
+    date_mod: '2023-01-26T19:56:43.000Z',
+    solvedate: null,
+    closedate: null,
+    users_id_recipient: 2,
+    status: 2,
+    priority: 3,
+    itilcategories_id: 0,
+    type: 2,
+    locations_id: 0,
+  }
+
   return (
     <>
       <div className="flex flex-col h-screen grid-cols-app">
@@ -73,10 +89,73 @@ export default function Home() {
             {/* className="grid md:grid-cols-4 grid-cols-card gap-4" */}
             {/* gap-7 grid flex-row flex-grow md:grid-cols-1 grid-cols-card */}
             <section className="grid-cols-3 gap-4 grid">
+              <CardGraph title="Chamados por Ano" />
+              <CardPie title="Chamados por Ano" />
               <CardGraph title="Chamados por Mês" />
-              <CardPie title="Chamados por Urgência" />
-              <CardGraph title="Chamados por Mês" />
-              <CardPie title="Chamados por Urgência" />
+              <CardPie title="Chamados por Mês" />
+              <CardGraph title="Chamados por Semana" />
+              <CardPie title="Chamados por Semana" />
+              <CardGraph title="Chamados por Dia" />
+              <CardPie title="Chamados por Dia" />
+            </section>
+
+            <section className="mt-4">
+              <div className="flex justify-center items-center bg-slate-100">
+                <div className="bg-white p-8 rounded-lg shadow-md w-full ">
+                  <h2 className="text-2xl font-semibold mb-6 text-gray-800 text-center">
+                    Ticket Details
+                  </h2>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Name:</span>{' '}
+                      {ticketData.name}
+                    </p>
+                  </div>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Status:</span>{' '}
+                      {ticketData.status}
+                    </p>
+                  </div>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Priority:</span>{' '}
+                      {ticketData.priority}
+                    </p>
+                  </div>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Type:</span>{' '}
+                      {ticketData.type}
+                    </p>
+                  </div>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Date Created:</span>{' '}
+                      {new Date(ticketData.date_creation).toLocaleDateString()}
+                    </p>
+                  </div>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">Last Modified:</span>{' '}
+                      {new Date(ticketData.date_mod).toLocaleDateString()}
+                    </p>
+                  </div>
+
+                  <div className="mb-4">
+                    <p className="text-gray-600">
+                      <span className="font-semibold">User ID Recipient:</span>{' '}
+                      {ticketData.users_id_recipient}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </section>
           </main>
         </div>
