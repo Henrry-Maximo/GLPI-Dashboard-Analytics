7b9e72b360014a5e11d2e7b31581ace36090be6e
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-10-02 16:40:39 -0300

    📦 Refactor: Add Link React Router Dom at NavItem for Monitoring page

--- GLPI-Analytics-Pro/src/components/Header/NavItem/NavItem.tsx
+++ GLPI-Analytics-Pro/src/components/Header/NavItem/NavItem.tsx
@@ -1,4 +1,5 @@
 import { ElementType } from 'react'
+import { Link } from 'react-router-dom'
 
 interface NavItemProps {
   icon: ElementType
@@ -7,14 +8,14 @@ interface NavItemProps {
 
 export function NavItem({ icon: Icon, route }: NavItemProps) {
   return (
-    <a
-      href={route}
+    <Link
+      to={route}
       className="group flex items-center gap-2 bg-slate-100 p-2 rounded hover:bg-white border border-white hover:border-orange-400 transition duration-300 ease-in-out"
     >
       <Icon
         size={24}
         className="h-5 w-5 text-gray-700 group-hover:text-orange-500 transition duration-300 ease-in-out"
       />
-    </a>
+    </Link>
   )
 }
