198657c9398cf4544f124cbe1c343471f927548c
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-10-14 12:45:25 -0300

    📚 Refactor: using Dayjs at real time with useState and useEffect

--- GLPI-Analytics-Pro/src/pages/monitoring/index.tsx
+++ GLPI-Analytics-Pro/src/pages/monitoring/index.tsx
@@ -6,33 +6,36 @@ import dayjs from 'dayjs'
 
 import ptBR from 'dayjs/locale/pt-br'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import { useState, useEffect } from 'react'
 
 dayjs.locale(ptBR)
 dayjs.extend(relativeTime)
 
 export default function MonitoringTicket() {
+  const [currentTime, setCurrentTime] = useState(dayjs())
+
   const { data } = useQuery({
     queryKey: ['monitoring'],
     queryFn: getMonitoring,
     staleTime: 1000 * 60,
-    refetchInterval: 1000 * 5, // atualizar a cada 10 segundos
-    refetchOnWindowFocus: true, // reconsultar quando a janela ganha foco
+    refetchInterval: 1000 * 5, // 10 segundos
+    refetchOnWindowFocus: true, // reconsultar janela em foco
   })
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(dayjs())
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [])
 
   if (!data) {
     return null
   }
 
-  const ticketFormattingCreatingNumbers = dayjs(data.date_creation).format(
-    'DD/MM/YYYY HH:mm:ss',
-  )
-
-  const ticketFormattingCreatingString = dayjs(data.date_creation).format(
-    'dddd, D [de] MMMM [de] YYYY',
-  )
-
   const date = dayjs(data.date_creation)
-  const ticketFormattingCreatingRelative = date.fromNow()
+  const messageWithoutData = '...'
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -42,33 +45,42 @@ export default function MonitoringTicket() {
         </div>
         <h2 className="text-3xl font-medium">Painel de Alerta</h2>
         <p className="flex flex-col">
-          <span className="text-xs">01/07/2024</span>
-          <span className="text-2xl font-semibold">14:37:51</span>
+          <span className="text-xs">{currentTime.format('DD/MM/YYYY')}</span>
+          <span className="text-2xl font-semibold">
+            {currentTime.format('HH:mm:ss')}
+          </span>
         </p>
       </header>
       <main className="bg-slate-100 flex flex-col flex-grow h-full text-center justify-center">
         <div className="mb-8">
           <h2 className="font-bold flex flex-row gap-2 text-3xl justify-center text-gray-600">
-            <span className="text-orange-500">[{data ? data.id : null}]</span>
+            <span className="text-orange-500">
+              [{data ? data.id : messageWithoutData}]
+            </span>
             Último Chamado
           </h2>
           <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
             Prioridade:
-            <span className="text-blue-500">{data ? data.priority : null}</span>
+            <span className="text-blue-500">
+              {data ? data.priority : messageWithoutData}
+            </span>
           </p>
           <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
             Status:
-            <span className="text-blue-500">{data ? data.status : null}</span>
+            <span className="text-blue-500">
+              {data ? data.status : messageWithoutData}
+            </span>
           </p>
         </div>
         <div className="mb-8">
           <h1 className="mb-2 font-bold flex flex-col gap-2 text-5xl justify-center text-gray-600">
-            {data ? data.firstname : null} {data ? data.realname : null}
+            {data ? data.firstname : messageWithoutData}{' '}
+            {data ? data.realname : messageWithoutData}
           </h1>
           <p className="font-medium flex flex-row gap-2 text-lg justify-center text-gray-600">
             Local de Atendimento:{' '}
             <span className="text-orange-500">
-              {data ? data.location : null}
+              {data ? data.location : messageWithoutData}
             </span>
           </p>
           {/* <p className="font-medium flex flex-row gap-2 text-lg justify-center text-gray-600">
@@ -78,18 +90,22 @@ export default function MonitoringTicket() {
         <div>
           <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
             Título do Chamado:
-            <span className="text-orange-500">{data ? data.title : null}</span>
+            <span className="text-orange-500">
+              {data ? data.title : messageWithoutData}
+            </span>
           </p>
           <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
             Data de Criação:
             <span className="text-orange-500">
-              {data ? ticketFormattingCreatingString : null}
+              {data
+                ? date.format('dddd, D [de] MMMM [de] YYYY')
+                : messageWithoutData}
             </span>
           </p>
           <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
             <span className="text-orange-500">
-              {data ? ticketFormattingCreatingNumbers : null} (
-              {data ? ticketFormattingCreatingRelative : null})
+              {data ? date.format('DD/MM/YYYY HH:mm') : messageWithoutData} (
+              {data ? date.fromNow() : messageWithoutData})
             </span>
           </p>
         </div>
