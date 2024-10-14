f30661953ca309b21f2bdd16b145242c269be2f8
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-10-14 15:22:43 -0300

    🎨 Style: Add form at Monitoring page using Tailwind

--- GLPI-Analytics-Pro/src/pages/monitoring/index.tsx
+++ GLPI-Analytics-Pro/src/pages/monitoring/index.tsx
@@ -40,74 +40,94 @@ export default function MonitoringTicket() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="flex flex-row text-center items-center gap-4 justify-between bg-orange-500 p-4 text-slate-100">
-        <div className="">
-          <NavItem icon={ArrowCircleLeft} route="/home" />
-        </div>
-        <h2 className="text-3xl font-medium">Painel de Alerta</h2>
-        <p className="flex flex-col">
+        <NavItem icon={ArrowCircleLeft} route="/home" />
+        <h2 className="text-4xl font-medium">Painel de Alerta</h2>
+        {/* relógio */}
+        <p className="flex flex-col items-end">
           <span className="text-xs">{currentTime.format('DD/MM/YYYY')}</span>
-          <span className="text-2xl font-semibold">
+          <span className="text-2xl font-semibold tabular-nums">
             {currentTime.format('HH:mm:ss')}
           </span>
         </p>
       </header>
-      <main className="bg-slate-100 flex flex-col flex-grow h-full text-center justify-center">
-        <div className="mb-8">
+      <main className="bg-slate-100 flex flex-col flex-grow h-full justify-between">
+        {/* container último chamado */}
+        <div className="flex flex-col text-center p-16">
           <h2 className="font-bold flex flex-row gap-2 text-3xl justify-center text-gray-600">
             <span className="text-orange-500">
               [{data ? data.id : messageWithoutData}]
             </span>
             Último Chamado
           </h2>
-          <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
-            Prioridade:
-            <span className="text-blue-500">
-              {data ? data.priority : messageWithoutData}
-            </span>
-          </p>
-          <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
-            Status:
-            <span className="text-blue-500">
-              {data ? data.status : messageWithoutData}
-            </span>
-          </p>
+          <div className="mt-4">
+            <h1 className="mb-2 font-bold flex flex-col gap-2 text-5xl justify-center text-gray-600">
+              {data ? data.firstname : messageWithoutData}{' '}
+              {data ? data.realname : messageWithoutData}
+            </h1>
+            <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
+              Status:
+              <span className="text-yellow-500">
+                {data ? data.status : messageWithoutData}
+              </span>
+            </p>
+            <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
+              Prioridade:
+              <span className="text-blue-500">
+                {data ? data.priority : messageWithoutData}
+              </span>
+            </p>
+            <p className="font-medium flex flex-row gap-2 text-lg justify-center text-gray-600">
+              Local de Atendimento:{' '}
+              <span className="text-orange-500">
+                {data ? data.location : messageWithoutData}
+              </span>
+            </p>
+          </div>
+          <div className="mt-4">
+            <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
+              Título do Chamado:
+              <span className="text-orange-500">
+                {data ? data.title : messageWithoutData}
+              </span>
+            </p>
+            <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
+              Data de Criação:
+              <span className="text-gray-600">
+                {data
+                  ? date.format('dddd, D [de] MMMM [de] YYYY')
+                  : messageWithoutData}
+              </span>
+            </p>
+            <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
+              <span className="text-gray-600">
+                {data ? date.format('DD/MM/YYYY HH:mm') : messageWithoutData} (
+                {data ? date.fromNow() : messageWithoutData})
+              </span>
+            </p>
+          </div>
         </div>
-        <div className="mb-8">
-          <h1 className="mb-2 font-bold flex flex-col gap-2 text-5xl justify-center text-gray-600">
-            {data ? data.firstname : messageWithoutData}{' '}
-            {data ? data.realname : messageWithoutData}
-          </h1>
-          <p className="font-medium flex flex-row gap-2 text-lg justify-center text-gray-600">
-            Local de Atendimento:{' '}
-            <span className="text-orange-500">
-              {data ? data.location : messageWithoutData}
-            </span>
-          </p>
-          {/* <p className="font-medium flex flex-row gap-2 text-lg justify-center text-gray-600">
-            Equipamento: <span className="text-orange-500">DSKSP139</span>
-          </p> */}
-        </div>
-        <div>
-          <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
-            Título do Chamado:
-            <span className="text-orange-500">
-              {data ? data.title : messageWithoutData}
-            </span>
-          </p>
-          <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
-            Data de Criação:
-            <span className="text-orange-500">
-              {data
-                ? date.format('dddd, D [de] MMMM [de] YYYY')
-                : messageWithoutData}
-            </span>
-          </p>
-          <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
-            <span className="text-orange-500">
-              {data ? date.format('DD/MM/YYYY HH:mm') : messageWithoutData} (
-              {data ? date.fromNow() : messageWithoutData})
-            </span>
-          </p>
+        {/* lista de chamados */}
+        <div className="bg-zinc-200 text-gray-600 p-4 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold mb-4 ml-8">
+            ... [Lista de Chamados] ...
+          </h2>
+          <ul className="list-disc list-inside">
+            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
+              Item 1
+            </li>
+            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
+              Item 2
+            </li>
+            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
+              Item 3
+            </li>
+            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
+              Item 4
+            </li>
+            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
+              Item 5
+            </li>
+          </ul>
         </div>
       </main>
     </div>
