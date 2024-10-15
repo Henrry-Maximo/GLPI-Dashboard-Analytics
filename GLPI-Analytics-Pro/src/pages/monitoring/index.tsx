eb8f65c346769a0ae2e31ce5507618d6aa0ee5e4
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-10-15 17:08:32 -0300

    ✨Feat: Add list at Monitoring page

--- GLPI-Analytics-Pro/src/pages/monitoring/index.tsx
+++ GLPI-Analytics-Pro/src/pages/monitoring/index.tsx
@@ -38,10 +38,10 @@ export default function MonitoringTicket() {
   const messageWithoutData = '...'
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="flex flex-row text-center items-center gap-4 justify-between bg-orange-500 p-4 text-slate-100">
+    <div className="h-screen flex flex-col">
+      <header className="flex flex-row items-center gap-4 justify-between bg-orange-500 p-4 text-slate-100">
         <NavItem icon={ArrowCircleLeft} route="/home" />
-        <h2 className="text-4xl font-medium">Painel de Alerta</h2>
+        <h2 className="text-4xl font-medium">MONITORAMENTO GLPI</h2>
         {/* relógio */}
         <p className="flex flex-col items-end">
           <span className="text-xs">{currentTime.format('DD/MM/YYYY')}</span>
@@ -50,9 +50,10 @@ export default function MonitoringTicket() {
           </span>
         </p>
       </header>
-      <main className="bg-slate-100 flex flex-col flex-grow h-full justify-between">
+
+      <main className="flex flex-col flex-grow">
         {/* container último chamado */}
-        <div className="flex flex-col text-center p-16">
+        <div className="flex flex-col flex-1  text-center justify-center rounded-sm bg-white shadow-md">
           <h2 className="font-bold flex flex-row gap-2 text-3xl justify-center text-gray-600">
             <span className="text-orange-500">
               [{data ? data.id : messageWithoutData}]
@@ -60,37 +61,28 @@ export default function MonitoringTicket() {
             Último Chamado
           </h2>
           <div className="mt-4">
-            <h1 className="mb-2 font-bold flex flex-col gap-2 text-5xl justify-center text-gray-600">
+            <h1 className="mb-2 font-bold text-5xl text-gray-600">
               {data ? data.firstname : messageWithoutData}{' '}
               {data ? data.realname : messageWithoutData}
             </h1>
-            <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
-              Status:
-              <span className="text-yellow-500">
-                {data ? data.status : messageWithoutData}
-              </span>
+            <p className="font-medium text-2xl text-gray-600">
+              Status: <span>{data ? data.status : messageWithoutData}</span>
             </p>
-            <p className="font-medium flex flex-row gap-2 text-2xl justify-center text-gray-600">
-              Prioridade:
-              <span className="text-blue-500">
-                {data ? data.priority : messageWithoutData}
-              </span>
+            <p className="font-medium text-2xl text-gray-600">
+              Prioridade:{' '}
+              <span>{data ? data.priority : messageWithoutData}</span>
             </p>
-            <p className="font-medium flex flex-row gap-2 text-lg justify-center text-gray-600">
+            <p className="font-medium text-lg text-gray-600">
               Local de Atendimento:{' '}
-              <span className="text-orange-500">
-                {data ? data.location : messageWithoutData}
-              </span>
+              <span>{data ? data.location : messageWithoutData}</span>
             </p>
           </div>
           <div className="mt-4">
-            <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
-              Título do Chamado:
-              <span className="text-orange-500">
-                {data ? data.title : messageWithoutData}
-              </span>
+            <p className="font-bold text-xl text-gray-600">
+              Título do Chamado:{' '}
+              <span>{data ? data.title : messageWithoutData}</span>
             </p>
-            <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
+            <p className="font-bold text-xl text-gray-600">
               Data de Criação:
               <span className="text-gray-600">
                 {data
@@ -98,7 +90,7 @@ export default function MonitoringTicket() {
                   : messageWithoutData}
               </span>
             </p>
-            <p className="font-bold flex flex-row gap-2 text-xl justify-center text-gray-600">
+            <p className="font-bold text-xl text-gray-600">
               <span className="text-gray-600">
                 {data ? date.format('DD/MM/YYYY HH:mm') : messageWithoutData} (
                 {data ? date.fromNow() : messageWithoutData})
@@ -106,26 +98,82 @@ export default function MonitoringTicket() {
             </p>
           </div>
         </div>
-        {/* lista de chamados */}
-        <div className="bg-zinc-200 text-gray-600 p-4 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold mb-4 ml-8">
-            ... [Lista de Chamados] ...
+
+        {/* lista de chamados com altura fixa e scroll */}
+        <div className="bg-zinc-200 p-4 shadow-md h-80 overflow-y-auto">
+          <h2 className="bg-white text-2xl font-bold mb-4 ml-8 inline-flex p-1 rounded-lg">
+            Últimas Chamadas
           </h2>
-          <ul className="list-disc list-inside">
-            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
-              Item 1
+          <ul className="list-none">
+            {/* Exemplo de item da lista */}
+            <li className="mb-2 p-4 bg-white rounded shadow hover:bg-slate-100 transition duration-200 flex justify-between items-center">
+              <span className="font-bold text-gray-700">ID: 001</span>
+              <span className="font-medium text-gray-600">
+                Paciente: João Silva
+              </span>
+              <span className="font-medium text-gray-600">
+                Título: Chamado de Teste
+              </span>
+              <span className="font-medium text-gray-600">
+                Local: Financeiro
+              </span>
+              <span className="font-semibold text-green-500">
+                Status: Concluído
+              </span>
             </li>
-            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
-              Item 2
+            <li className="mb-2 p-4 bg-white rounded shadow hover:bg-slate-100 transition duration-200 flex justify-between items-center">
+              <span className="font-bold text-gray-700">ID: 002</span>
+              <span className="font-medium text-gray-600">
+                Paciente: Maria Oliveira
+              </span>
+              <span className="font-medium text-gray-600">
+                Título: Erro no Sistema
+              </span>
+              <span className="font-medium text-gray-600">
+                Local: Comercial
+              </span>
+              <span className="font-semibold text-yellow-500">
+                Status: Em andamento
+              </span>
             </li>
-            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
-              Item 3
+            <li className="mb-2 p-4 bg-white rounded shadow hover:bg-slate-100 transition duration-200 flex justify-between items-center">
+              <span className="font-bold text-gray-700">ID: 003</span>
+              <span className="font-medium text-gray-600">
+                Paciente: Pedro Lima
+              </span>
+              <span className="font-medium text-gray-600">
+                Título: Solicitação de Acesso
+              </span>
+              <span className="font-medium text-gray-600">Local: TI</span>
+              <span className="font-semibold text-red-500">
+                Status: Pendente
+              </span>
             </li>
-            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
-              Item 4
+            <li className="mb-2 p-4 bg-white rounded shadow hover:bg-slate-100 transition duration-200 flex justify-between items-center">
+              <span className="font-bold text-gray-700">ID: 003</span>
+              <span className="font-medium text-gray-600">
+                Paciente: Pedro Lima
+              </span>
+              <span className="font-medium text-gray-600">
+                Título: Solicitação de Acesso
+              </span>
+              <span className="font-medium text-gray-600">Local: TI</span>
+              <span className="font-semibold text-red-500">
+                Status: Pendente
+              </span>
             </li>
-            <li className="mb-2 p-2 bg-orange-500 rounded hover:bg-slate-100 transition duration-200">
-              Item 5
+            <li className="mb-2 p-4 bg-white rounded shadow hover:bg-slate-100 transition duration-200 flex justify-between items-center">
+              <span className="font-bold text-gray-700">ID: 003</span>
+              <span className="font-medium text-gray-600">
+                Paciente: Pedro Lima
+              </span>
+              <span className="font-medium text-gray-600">
+                Título: Solicitação de Acesso
+              </span>
+              <span className="font-medium text-gray-600">Local: TI</span>
+              <span className="font-semibold text-red-500">
+                Status: Pendente
+              </span>
             </li>
           </ul>
         </div>
--- api-rest-GLPI/src/server.ts
+++ api-rest-GLPI/src/server.ts
@@ -3,7 +3,6 @@ import { env } from "./env";
 
 app
   .listen({
-    host: "0.0.0.0",
     port: env.NODE_PORT,
   })
   .then(() => {
