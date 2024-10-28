2e64aa895d0164cf178ecb3f6c58321a90bf772c
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-10-28 16:55:58 -0300

    ⚙ Chore: Remove files

--- GLPI-Analytics-Pro/src/http/get-monitoring.ts
+++
@@ -1,8 +0,0 @@
-import type { MonitoringResponse } from '../@types/interface-monitoring'
-
-export async function getMonitoring(): Promise<MonitoringResponse> {
-  const response = await fetch('http://localhost:5000/api-glpi/tickets/last')
-  const data = await response.json()
-
-  return data
-}
