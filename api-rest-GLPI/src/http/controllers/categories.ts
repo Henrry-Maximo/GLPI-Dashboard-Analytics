dd78cf728480fff426886cc823d828180e8d0af5
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-12-09 13:43:38 -0300

    ✨Feat: Add controller at routes

--- api-rest-GLPI/src/http/controllers/categories.ts
+++ api-rest-GLPI/src/http/controllers/categories.ts
@@ -2,16 +2,16 @@ import { FastifyInstance } from "fastify";
 import { getCategories } from "@/use-cases/get-categories";
 import { categoriesByCount } from "@/use-cases/categories-by-count";
 
-export async function categorieController(app: FastifyInstance) {
+export async function categoriesController(app: FastifyInstance) {
   // retornar nome de todas as categorias
-  app.get("/categories", async (req, reply) => {
+  app.get("/categories", async (_, reply) => {
     const { categories } = await getCategories();
 
     return reply.status(200).send({ categories })
   });
 
   // retornar total de categorias
-  app.get("/categories-by-count", async (req, reply) => {
+  app.get("/categories-by-count", async (_, reply) => {
     const { categoriesQuantity } = await categoriesByCount();
 
     return reply.status(200).send({ categoriesQuantity })
--- api-rest-GLPI/src/http/routes.ts
+++ api-rest-GLPI/src/http/routes.ts
@@ -4,10 +4,12 @@ import { login } from "./controllers/login";
 import { register } from "./controllers/register";
 
 import { ticketController } from "./controllers/tickets";
+import { categoriesController } from "./controllers/categories";
 
 export async function routes(app: FastifyInstance) {
   app.post("/sessions", login)
   app.post("/register", register)
 
   app.register(ticketController, { prefix: "tickets" });
+  app.register(categoriesController, { prefix: "categories" });
 }
