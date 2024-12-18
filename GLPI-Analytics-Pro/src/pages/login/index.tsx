321e2f7e00c45692cf19137c15e3c197c5d79bb6
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-12-18 10:46:35 -0300

    🎨 Style: Add animate ping tailwind at display error

--- GLPI-Analytics-Pro/src/pages/login/index.tsx
+++ GLPI-Analytics-Pro/src/pages/login/index.tsx
@@ -98,7 +98,12 @@ export default function Index() {
           </button>
 
           {errorMessage && (
-            <p className="bg-red-600 text-white p-2 border-x-2 rounded-md">
+            <p className="flex gap-2 items-center bg-gray-500 text-white p-2 border-x-2 rounded-md">
+              <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-orange-500 opacity-75" />
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-orange-500" />
+              </span>
+
               {errorMessage}
             </p>
           )}
