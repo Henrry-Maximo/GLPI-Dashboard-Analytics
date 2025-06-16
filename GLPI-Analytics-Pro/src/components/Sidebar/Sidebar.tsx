1e7bfb6e1aa6153260b32bf18cafa3271e5caa68
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-06-16 16:59:26 -0300

    🐍 Fix: Add adjust at sidebar operation for open and closed

--- GLPI-Analytics-Pro/src/components/Sidebar/Sidebar.tsx
+++ GLPI-Analytics-Pro/src/components/Sidebar/Sidebar.tsx
@@ -18,28 +18,52 @@ import { NavigationMenuLink } from "./Link";
 // remover token e voltar para login
 function handleLogout() {
   const navigate = useNavigate();
-  
-  sessionStorage.removeItem("jwt"); 
+
+  sessionStorage.removeItem("jwt");
   navigate("/login");
 }
 
 interface PropsSidebarHandler {
-  disabled: boolean;
+  disabled?: boolean;
 }
 
 export const Sidebar = ({ disabled }: PropsSidebarHandler) => {
   return (
     <aside
-      className={`${
-        disabled ? "w-52 px-5 py-8" : "w-0 invisible"
-      } flex flex-col bg-gray-50 border-r border-gray-300 transition-all duration-200 ease-in-out`}
+      className={`flex flex-col bg-gray-50 border-r border-gray-300 transition-all duration-300 ease-in-out
+    ${
+      disabled
+        ? "w-52 px-5 py-8 visible"
+        : "w-0 px-0 py-0 overflow-hidden invisible"
+    }
+  `}
     >
       <nav className="space-y-0.5">
-        <NavigationMenuLink icon={PresentationChart} title="Dashboard" path="/main/home" />
-        <NavigationMenuLink icon={Heartbeat} title="Monitoramento" path="/monitoring" />
-        <NavigationMenuLink icon={Ticket} title="Chamados" path="/main/tickets" />
-        <NavigationMenuLink icon={TrendUp} title="Estatísticas" path="/main/statistics" />
-        <NavigationMenuLink icon={ChartPie} title="Análises" path="/main/analytics" />
+        <NavigationMenuLink
+          icon={PresentationChart}
+          title="Dashboard"
+          path="/main/home"
+        />
+        <NavigationMenuLink
+          icon={Heartbeat}
+          title="Monitoramento"
+          path="/monitoring"
+        />
+        <NavigationMenuLink
+          icon={Ticket}
+          title="Chamados"
+          path="/main/tickets"
+        />
+        <NavigationMenuLink
+          icon={TrendUp}
+          title="Estatísticas"
+          path="/main/statistics"
+        />
+        <NavigationMenuLink
+          icon={ChartPie}
+          title="Análises"
+          path="/main/analytics"
+        />
         <NavigationMenuLink
           icon={BellSimple}
           title="Notificações"
@@ -50,7 +74,11 @@ export const Sidebar = ({ disabled }: PropsSidebarHandler) => {
 
       <div className="mt-auto">
         <nav className="space-y-0.5">
-          <NavigationMenuLink icon={Gear} title="Configuração" path="/main/system" />
+          <NavigationMenuLink
+            icon={Gear}
+            title="Configuração"
+            path="/main/system"
+          />
 
           <NavigationMenuLink
             icon={SignOut}
@@ -64,4 +92,4 @@ export const Sidebar = ({ disabled }: PropsSidebarHandler) => {
       </div>
     </aside>
   );
-}
+};
--- GLPI-Analytics-Pro/src/layout/DefaultLayout/index.tsx
+++ GLPI-Analytics-Pro/src/layout/DefaultLayout/index.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Header } from '../../components/Header/Header';
-import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Header } from "../../components/Header/Header";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 
 export const DefaultLayout = () => {
   // estado para controle de abertura e fechamento da sidebar
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [show, setShow] = useState(true);
+
+  // const saved = localStorage.getItem("sidebar");
+  // return saved === null ? true : JSON.parse(saved);
+
+  // useEffect(() => {
+  //   localStorage.setItem('sidebar', JSON.stringify(show));
+    
+    // const statusSidebar = localStorage.getItem('statusSidebar');
+    // console.log(statusSidebar);
+    // console.log("mostrar/ocultar");
+  // }, [show]);
 
   function toggleSidebar() {
-    setIsSidebarOpen(!isSidebarOpen);
+    setShow(!show);
   }
 
   return (
@@ -16,8 +27,8 @@ export const DefaultLayout = () => {
       <div className="col-span-full">
         <Header toggleSidebar={() => toggleSidebar()} />
       </div>
-      
-      <Sidebar disabled={isSidebarOpen}  />
+
+      <Sidebar disabled={show} />
 
       {/* renderizar conteúdo das páginas de acordo com a rota */}
       <div className="flex flex-1 overflow-y-auto p-4">
@@ -25,4 +36,4 @@ export const DefaultLayout = () => {
       </div>
     </div>
   );
-}
+};
