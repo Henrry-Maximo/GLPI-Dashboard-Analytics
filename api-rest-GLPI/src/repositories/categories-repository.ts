1000cfbdbb649272c5e2a08251b037739d7cf19a
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-08-25 12:29:40 -0300

    ✨ Feat: Add data categories relationship with tickets

--- api-rest-GLPI/src/repositories/categories-repository.ts
+++ api-rest-GLPI/src/repositories/categories-repository.ts
@@ -3,19 +3,17 @@ export interface CategoriesTicketsSchema {
     total: number;
     in_use: number;
     unused: number;
-  }
+  },
   result: [
     {
       id: number;
       name: string;
-      type: number;
       amount_tickets: number;
-      author: string;
       date_creation: string;
     }
   ]
 }
 
 export interface CategoriesRepository {
-  get(): Promise<CategoriesTicketsSchema>
+  get(): Promise<CategoriesTicketsSchema | null>
 }
--- api-rest-GLPI/src/repositories/knex/knex-categories-repository.ts
+++ api-rest-GLPI/src/repositories/knex/knex-categories-repository.ts
@@ -1,7 +1,49 @@
-import { CategoriesRepository, CategoriesTicketsSchema } from "../categories-repository";
+import { knex } from "@/database/knex-config";
+import { CategoriesRepository } from "../categories-repository";
 
 export class KnexCategoriesRepository implements CategoriesRepository {
-  get(): Promise<CategoriesTicketsSchema> {
-    throw new Error("Method not implemented.");
+  async get(): Promise<any> {
+    const data = await knex("glpi_tickets")
+      .select(
+        "glpi_itilcategories.id",
+        "glpi_itilcategories.name",
+        "glpi_itilcategories.date_creation"
+      )
+      .count("glpi_tickets.id as amount_tickets")
+      .join(
+        "glpi_itilcategories",
+        "glpi_tickets.itilcategories_id",
+        "glpi_itilcategories.id"
+      )
+      // .whereBetween("glpi_tickets.date_creation", [
+      //   "2025-08-01",
+      //   "2025-08-31",
+      // ])
+      .groupBy("glpi_itilcategories.completename")
+      .orderBy("glpi_itilcategories.id")
+      .limit(20);
+
+    if (!data.length) {
+      return null;
+    }
+
+    const total = data.length;
+    // const inUse = await knex("glpi_tickets")
+    //   .count("glpi_tickets.id as amount_tickets")
+    //   .join(
+    //     "glpi_itilcategories",
+    //     "glpi_tickets.itilcategories_id",
+    //     "glpi_itilcategories.id"
+    //   )
+    //   .havingRaw("COUNT(glpi_tickets.id) > 0");
+
+    return {
+      meta: {
+        total,
+        in_use: 10,
+        unused: 20,
+      },
+      result: data,
+    };
   }
 }
