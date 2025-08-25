71eeafa23c3bc630285592aaf14ff76322a92ae0
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-08-25 16:48:23 -0300

    🐍 Fix: Add adjust in relationship between tickets and categories total, and total use and total unused

--- api-rest-GLPI/src/repositories/knex/knex-categories-repository.ts
+++ api-rest-GLPI/src/repositories/knex/knex-categories-repository.ts
@@ -20,28 +20,33 @@ export class KnexCategoriesRepository implements CategoriesRepository {
       //   "2025-08-31",
       // ])
       .groupBy("glpi_itilcategories.completename")
-      .orderBy("glpi_itilcategories.id")
-      .limit(20);
+      .orderBy("glpi_itilcategories.id");
 
-    if (!data.length) {
-      return null;
-    }
+    const inUse = await knex("glpi_tickets")
+      .count("glpi_itilcategories.id")
+      .join(
+        "glpi_itilcategories",
+        "glpi_tickets.itilcategories_id",
+        "glpi_itilcategories.id"
+      )
+      .groupBy("glpi_itilcategories.id")
+      .havingRaw("COUNT(glpi_tickets.id) > 0");
+
+    const unUsed = await knex("glpi_itilcategories")
+      .leftJoin("glpi_tickets", "glpi_itilcategories.id", "glpi_tickets.itilcategories_id")
+      .count("glpi_tickets.id as total_tickets")
+      .groupBy("glpi_itilcategories.id")
+      .havingRaw("COUNT(glpi_tickets.id) = 0");
 
     const total = data.length;
-    // const inUse = await knex("glpi_tickets")
-    //   .count("glpi_tickets.id as amount_tickets")
-    //   .join(
-    //     "glpi_itilcategories",
-    //     "glpi_tickets.itilcategories_id",
-    //     "glpi_itilcategories.id"
-    //   )
-    //   .havingRaw("COUNT(glpi_tickets.id) > 0");
+    const totalInUSE = inUse.length;
+    const unused = unUsed.length;
 
     return {
       meta: {
         total,
-        in_use: 10,
-        unused: 20,
+        in_use: totalInUSE,
+        unused: unused,
       },
       result: data,
     };
