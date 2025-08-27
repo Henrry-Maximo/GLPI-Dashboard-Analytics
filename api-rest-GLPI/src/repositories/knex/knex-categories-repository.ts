a72f595d64303adb191893ac565b5436d3bf1f64
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-08-27 15:35:42 -0300

    🐍 Fix: Add adjust in querys and filter date

--- api-rest-GLPI/src/repositories/knex/knex-categories-repository.ts
+++ api-rest-GLPI/src/repositories/knex/knex-categories-repository.ts
@@ -4,75 +4,63 @@ import {
   CategoriesTicketsSchema,
   FiltersCategoriesSchema,
 } from "../categories-repository";
-import { format, startOfMonth } from "date-fns";
+import { format, startOfMonth, endOfMonth } from "date-fns";
 
 export class KnexCategoriesRepository implements CategoriesRepository {
-  // ❗ Todo: Performance inefficiencies detected in code.
   async get({
     start_date,
     end_date,
   }: FiltersCategoriesSchema): Promise<CategoriesTicketsSchema> {
-    const date = new Date();
-    const firstDayMonth = startOfMonth(date);
+    const today = new Date();
+    const firstDayMonth = format(startOfMonth(today), "yyyy-MM-dd");
+    const lastDayMonth = format(endOfMonth(today), "yyyy-MM-dd");
 
-    const [amountCategoriesInUse, amountCategoriesUnUsed] = await Promise.all([
-      knex("glpi_itilcategories")
-        .countDistinct({ total: "glpi_itilcategories.id" })
-        .leftJoin(
-          "glpi_tickets",
-          "glpi_itilcategories.id",
-          "glpi_tickets.itilcategories_id"
-        )
-        .whereBetween("glpi_tickets.date_creation", [
-          start_date ?? format(date, "yyyy-MM-dd"),
-          end_date ?? format(firstDayMonth, "yyyy-MM-dd"),
-        ])
-        .whereNotNull("glpi_tickets.id")
-        .first(),
-      knex("glpi_itilcategories")
-        .count({ total: "glpi_itilcategories.id" })
-        .leftJoin(
-          "glpi_tickets",
-          "glpi_itilcategories.id",
-          "glpi_tickets.itilcategories_id"
-        )
-        .whereBetween("glpi_tickets.date_creation", [
-          start_date ?? format(date, "yyyy-MM-dd"),
-          end_date ?? format(firstDayMonth, "yyyy-MM-dd"),
-        ])
-        .whereNull("glpi_tickets.id")
-        .first(),
-    ]);
+    const startDate = start_date ?? firstDayMonth;
+    const endDate = end_date ?? lastDayMonth;
 
-    let query = knex("glpi_tickets")
-      .select(
-        "glpi_itilcategories.id",
-        "glpi_itilcategories.name",
-        "glpi_itilcategories.date_creation"
-      )
-      .count({ total: "glpi_tickets.id" })
-      .join(
-        "glpi_itilcategories",
-        "glpi_tickets.itilcategories_id",
-        "glpi_itilcategories.id"
-      )
-      .groupBy("glpi_itilcategories.completename")
-      .orderBy("glpi_itilcategories.id");
+    const [amountCategoriesInUse, amountCategoriesUnUsed, data] =
+      await Promise.all([
+        knex("glpi_itilcategories")
+          .countDistinct({ total: "glpi_itilcategories.id" })
+          .leftJoin(
+            "glpi_tickets",
+            "glpi_itilcategories.id",
+            "glpi_tickets.itilcategories_id"
+          )
+          .whereBetween("glpi_tickets.date_creation", [startDate, endDate])
+          .whereNotNull("glpi_tickets.id")
+          .first(),
 
-    if (start_date && end_date) {
-      query = query.whereBetween("glpi_tickets.date_creation", [
-        start_date,
-        end_date,
+        knex("glpi_itilcategories")
+          .count({ total: "glpi_itilcategories.id" })
+          .leftJoin(
+            "glpi_tickets",
+            "glpi_itilcategories.id",
+            "glpi_tickets.itilcategories_id"
+          )
+          .whereBetween("glpi_tickets.date_creation", [startDate, endDate])
+          .whereNull("glpi_tickets.id")
+          .first(),
+
+        knex("glpi_tickets")
+          .select(
+            "glpi_itilcategories.id",
+            "glpi_itilcategories.name",
+            "glpi_itilcategories.date_creation"
+          )
+          .count({ total: "glpi_tickets.id" })
+          .leftJoin(
+            "glpi_itilcategories",
+            "glpi_tickets.itilcategories_id",
+            "glpi_itilcategories.id"
+          )
+          .whereBetween("glpi_tickets.date_creation", [startDate, endDate])
+          .groupBy("glpi_itilcategories.completename")
+          .orderBy("glpi_itilcategories.id"),
       ]);
-    } else if (start_date) {
-      query = query.where("glpi_tickets.date_creation", ">=", start_date);
-    } else if (end_date) {
-      query = query.where("glpi_tickets.date_creation", "<=", end_date);
-    }
 
     const totalInUse: number = Number(amountCategoriesInUse?.total ?? 0);
     const totalUnUsed: number = Number(amountCategoriesUnUsed?.total ?? 0);
-
     const total = totalInUse + totalUnUsed;
 
     return {
@@ -81,7 +69,7 @@ export class KnexCategoriesRepository implements CategoriesRepository {
         in_use: totalInUse,
         unused: totalUnUsed,
       },
-      result: await query,
+      result: data,
     };
   }
 }
