910aa45d6293e4a3608ca1e03e5b7f80e34997ce
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-09-02 16:15:51 -0300

    ✨ Feat: Add test for get categories

--- api-rest-GLPI/src/use-cases/get-categories.spec.ts
+++ api-rest-GLPI/src/use-cases/get-categories.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import { GetCategoriesUseCase } from "./get-categories";
 
 describe("Get Categories Use Case", () => {
-  it.skip("Should be able get categories", async () => {
+  it("Should be able get categories", async () => {
     const statsRepository = new InMemoryCategoriesRepository();
     const sut = new GetCategoriesUseCase(statsRepository);
 
@@ -14,15 +14,21 @@ describe("Get Categories Use Case", () => {
 
     expect(meta).toEqual(
       expect.objectContaining({
-
+        total: expect.any(Number),
+        in_use: expect.any(Number),
+        unused: expect.any(Number),
       })
     );
 
-    expect(result).toEqual(
+    expect(result).toEqual(expect.arrayContaining([
       expect.objectContaining({
-
+        id: expect.any(Number),
+        name: expect.any(String),
+        description: expect.any(String),
+        created_at: expect.any(Date),
+        updated_at: expect.any(Date),
       })
-    );
+    ]));
   });
 
   it.skip("Should be able to return categories list empty", async () => {});
