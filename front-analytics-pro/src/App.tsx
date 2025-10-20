0a3d996c502856f0d64fe6f4df744713722e3780
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-10-19 23:46:15 -0300

    🐛  Fix: Remove import desnecessary in app

--- front-analytics-pro/src/App.tsx
+++ front-analytics-pro/src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routers } from "./routes";
-import { TooltipProvider } from "./components/ui/tooltip";
 
 export const App = () => {
   return (
-    
     <BrowserRouter>
       <Routers />
     </BrowserRouter>
